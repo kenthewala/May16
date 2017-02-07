5a3d2a7b7490b2345c8c155d2ed69a67cf423ff3
anoop <anoopsinghkhimani@gmail.com>
2017-02-07 14:35:57 +0530

    CSS and JS files

--- src/main/webapp/resources/js/mcq.js
+++ src/main/webapp/resources/js/mcq.js
@@ -2,112 +2,96 @@
  * 
  */
 var noOfQuestions;
-var currentIndex=0;
-var questionBank = [ {
-	"question" : "Question 1",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answered":false,
-	"answer" : 1
-}, {
-	"question" : "Question 2",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answered":false,
-	"answer" : 1
-}, {
-	"question" : "Question 3",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answered":false,
-	"answer" : 1
-}, {
-	"question" : "Question 4",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answered":false,
-	"answer" : 1
-}, {
-	"question" : "Question 5",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answered":false,
-	"answer" : 1
-}, {
-	"question" : "Question 6",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answered":false,
-	"answer" : 1
-}, {
-	"question" : "Question 7",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answered":false,
-	"answer" : 1
-}, {
-	"question" : "Question 8",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answer" : 1
-}, {
-	"question" : "Question 9",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answer" : 1
-}, {
-	"question" : "Question 10",
-	"option1" : "Option A",
-	"option2" : "Option B",
-	"option3" : "Option C",
-	"option4" : "Option D",
-	"answer" : 1
-} ]
+var currentIndex = 0;
+var questionBankArr;
+var wrongAnswers=0;
+var correctAnswers=0;
+var userAnswer = [];
 
 window.onload = function() {
-	processQB(questionBank);
+	var actual_JSON;
+	loadJSON(function(response) {
+		// Parse JSON string into object
+		actual_JSON = JSON.parse(response);
+	});
+	processQB(actual_JSON);
+};
+
+function processQB(objectArr) {
+	questionBankArr = objectArr.questions;
+	noOfQuestions = questionBankArr.length;
+	var link = '';
+
+	for (var i = 1; i <= noOfQuestions; i++) {
+		link += '<div id=nav' + i.toString() + ' class=notAttempted onclick=showNewQuestion('
+				+ (i.toString() - 1) + ')>' + i.toString() + '</div>';
+	}
+	document.getElementById("attempted").innerHTML = link;
+
+	for (var i = 0; i < noOfQuestions; i++) {
+		userAnswer.push({
+	        answered: false,
+	        selectedIndex: -1
+	    });
+	}
+	showNewQuestion(currentIndex);
 }
 
-function processQB(arr){
-	noOfQuestions = arr.length;
-	var scroll = '';
-	
-	for(var i=1;i<=noOfQuestions;i++){
-		scroll += i.toString()+" ";
+function showNewQuestion(newIndex) {
+	if (newIndex > questionBankArr.length || newIndex < 0)
+		return true;
+
+	currentIndex = newIndex;
+
+	document.getElementById("questionText").innerHTML = questionBankArr[newIndex].question;
+
+	for (var i = 1; i <= 4; i++) {
+		var quest = questionBankArr[newIndex];
+		document.getElementById("option" + i.toString()).value = quest["option"
+				+ i.toString()];
+		document.getElementById("lblOption" + i.toString()).innerHTML = quest["option"
+				+ i.toString()];
+		
+	}
+	if(userAnswer[newIndex].answered){
+		document.getElementsByName("option")[userAnswer[newIndex].selectedIndex].checked = true;
+	}else{
+		var radList = document.getElementsByName('option');
+	    for (var i = 0; i < radList.length; i++) {
+	      if(radList[i].checked) document.getElementById(radList[i].id).checked = false;
+	    }
 	}
 	
-	document.getElementById("attempted").innerHTML = scroll;
-	
-	document.getElementById("questionText").innerHTML = arr[currentIndex].question;
-	
-	document.getElementById("option1").value = arr[currentIndex].option1;
-	document.getElementById("lblOption1").innerHTML = arr[currentIndex].option1;
-	
-	console.log(document.getElementById("option2"),"option2");
-	document.getElementById("option2").value = arr[currentIndex].option2;
-	document.getElementById("lblOption2").innerHTML = arr[currentIndex].option2;
-	
-	document.getElementById("option3").value = arr[currentIndex].option3;
-	document.getElementById("lblOption3").innerHTML = arr[currentIndex].option3;
-	
-	document.getElementById("option4").value = arr[currentIndex].option4;
-	document.getElementById("lblOption4").innerHTML = arr[currentIndex].option4;
+	document.getElementById("questionIndex").innerHTML = "Question "+ (newIndex+1).toString()+ "/"+noOfQuestions
+}
+
+
+
+function markAttempted(choice){
+	document.getElementById("nav"+(currentIndex+1).toString()).className = "attempted";
+	userAnswer[currentIndex].answered = true;
+	userAnswer[currentIndex].selectedIndex = choice;
+	if(questionBankArr[currentIndex].answer == choice){
+		correctAnswers++;
+	}else{
+		wrongAnswers++;
+	}
+}
+
+function loadJSON(callback) {
+
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', 'resources/json/questionBank.json', false); // Replace
+																	// 'my_data'
+	// with the path to your
+	// file
+	xobj.onreadystatechange = function() {
+		if (xobj.readyState == 4 && xobj.status == "200") {
+			// Required use of an anonymous callback as .open will NOT return a
+			// value but simply returns undefined in asynchronous mode
+			callback(xobj.responseText);
+		}
+	};
+	xobj.send(null);
 }
